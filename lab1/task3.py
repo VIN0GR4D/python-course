d995158528dd2e7d3e223346a58eabe99347e516
VIN0GR4D <90242611+VIN0GR4D@users.noreply.github.com>
2023-10-18 00:22:22 +0300

    Update task3.py

--- lab1/task3.py
+++ lab1/task3.py
@@ -1,6 +1,10 @@
-total_bytes = 1.44 * 1024 * 1024
-bytes_for_book = 4 * 25 * 50 * 100
+BYTES_ONE_CHAR = 4
+pages = 100
+lines = 50
+chars = 25
 
-books_number = round(total_bytes / bytes_for_book)
+bytes_for_book = pages * lines * chars * BYTES_ONE_CHAR
+total_bytes = 1.44 * 1024 * 1024
+books_number = int(total_bytes // bytes_for_book)
 
 print("Количество книг, помещающихся на дискету:", books_number)
